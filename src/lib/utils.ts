3713c395d4f5d07e2daa82466da0d07487651a42
adr!an <111903096+adrianvrj@users.noreply.github.com>
2025-10-09 15:25:10 -0600

    deposit on vesu function
    TODO: withdraw function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { AegisSDK } from "@cavos/aegis";
+import { formatAmount } from "cavos-service-sdk";
+import { WBTC_ADDRESS, vWBTC_ADDRESS } from "../config/contracts";
+import { BTC_DECIMALS } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export async function depositVesu(account: AegisSDK, amount: number) {
+  const approve = {
+    contractAddress: WBTC_ADDRESS,
+    entrypoint: 'approve',
+    calldata: [account.address, formatAmount(amount, BTC_DECIMALS)],
+  }
+
+  const deposit = {
+    contractAddress: vWBTC_ADDRESS,
+    entrypoint: 'deposit',
+    calldata: [formatAmount(amount, BTC_DECIMALS), account.address],
+  }
+  const tx = await account.executeBatch([approve, deposit]);
+  return tx.transactionHash;
 }

0acd4a5f78c61a191feaba09860561a299c4bae0
jimenezz22 <ljimenezbogantes@gmail.com>
2025-10-15 11:48:30 -0600

    Refactor code structure for improved readability and maintainability

--- src/config/manifest.ts
+++ src/config/manifest.ts
@@ -1,6 +1,8 @@
-import mainnet from "../config/manifest_mainnet.json";
+import mainnetManifest from "../config/manifest_mainnet.json";
+import sepoliaManifest from "../config/manifest_sepolia.json";
+import { network } from "./cavosConfig";
 
-// Export the mainnet manifest
-export const manifest = mainnet;
+// Export the correct manifest based on network configuration
+export const manifest = network === 'SN_MAINNET' ? mainnetManifest : sepoliaManifest;
 
 export type Manifest = typeof manifest;
